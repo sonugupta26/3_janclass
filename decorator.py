c31dc6d463bc044663bf52ec848abe8b04864c13
sonu <dearsonugupta26@gmail.com>
2023-02-01 21:42:49 +0545

    first commit

---
+++ decorator.py
@@ -0,0 +1,31 @@
+# def decor(func):
+#     def inner():
+#        a = func()
+#        add = a + 5
+#        return add
+#     return inner()
+# @decor
+# def num():
+#     return 10
+
+
+
+# def decor(func):
+#     def inner():
+#         b = func()
+#         sub = b - 10
+#         return sub
+#     return inner()
+
+# @decor
+# def num():
+#     return 20
+# print(num())
+
+
+def addition():
+    num1 = 5
+    num2 = 8
+    result = num1+num2
+    print(result)
+
---
+++ multi_value.py
@@ -0,0 +1,8 @@
+def add(y):
+    x = 20
+    c = x + y
+    d = x - y
+    return c, d
+sum, sub = add(10)
+print(sum)
+print(sub)
